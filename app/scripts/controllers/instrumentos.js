efeda42cbc281557b3627214792f03f64d8acef0
Marco Gómez <marcogonzalo@gmail.com>
2015-06-03 00:07:37 -0430

    maqueta de lista de instrumentos

---
+++ app/scripts/controllers/instrumentos.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sedadApp.controller:InstrumentosCtrl
+ * @description
+ * # InstrumentosCtrl
+ * Controller of the sedadApp
+ */
+angular.module('sedadApp')
+  .controller('InstrumentosCtrl', ['$scope', function ($scope) {
+    $scope.instrumentosDisponibles = [
+      {
+        'id': '1',
+        'nombre': 'Instrumento 1',
+        'updated_at': '2015-05-14T09:00:00'
+      },
+      {
+        'id': '2',
+        'nombre': 'Instrumento 2',
+        'updated_at': '2015-05-14T09:00:00'
+      },
+      {
+        'id': '3',
+        'nombre': 'Instrumento 3',
+        'updated_at': '2015-05-14T09:00:00'
+      },
+      {
+        'id': '4',
+        'nombre': 'Instrumento 4',
+        'updated_at': '2015-05-14T09:00:00'
+      },
+      {
+        'id': '5',
+        'nombre': 'Instrumento 5',
+        'updated_at': '2015-05-14T09:00:00'
+      },
+      {
+        'id': '6',
+        'nombre': 'Instrumento 6',
+        'updated_at': '2015-05-14T09:00:00'
+      },
+      {
+        'id': '7',
+        'nombre': 'Instrumento 7',
+        'updated_at': '2015-05-14T09:00:00'
+      },
+      {
+        'id': '8',
+        'nombre': 'Instrumento 8',
+        'updated_at': '2015-05-14T09:00:00'
+      }
+    ];
+    angular.forEach($scope.instrumentosDisponibles, function(instrumentosDisponible) {
+      instrumentosDisponible.rank = Math.random();
+    });
+  }]);
