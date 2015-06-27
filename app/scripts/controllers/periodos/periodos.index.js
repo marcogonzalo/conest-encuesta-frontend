b052bde95b15f385bed0efa4523972f954ae7d70
Monica Oviedo <oviedo.monica123@gmail.com>
2015-06-27 18:37:02 +0000

    eliminar un periodo

--- app/scripts/controllers/periodos/periodos.index.js
+++ app/scripts/controllers/periodos/periodos.index.js
@@ -5,4 +5,15 @@ angular.module('sedadApp')
 		$scope.periodos = Periodo.query();
 
 		console.log($scope.periodos);
+		
+		$scope.eliminarPeriodo = function(periodo){
+			periodo.$delete(function(){
+				for(var i=0, n=$scope.periodos.lenght; i<n; ++i){
+					if($scope.periodos[i].id === periodo.id){
+						$scope.periodos.splice(i, 1);
+						break;
+					}
+				}
+			});
+		};
 	}]);
