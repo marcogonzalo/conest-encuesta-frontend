2a7c43bff00a4f3048dc0733d18b5b3103ae0ed6
Marco Gómez <marcogonzalo@gmail.com>
2015-07-13 23:43:32 -0430

    registrar respuestas para una consulta

--- app/scripts/controllers/consultas/consultas.responder.js
+++ app/scripts/controllers/consultas/consultas.responder.js
@@ -23,11 +23,14 @@ angular.module('sedadApp')
     $http.get(SEDAD_API_V1_URL + '/consultas/' + $stateParams.id)
     	.success(function(data, status, headers, config) {
     		console.log(data);
-    		console.log("Consulta respondida");
 		
-		    var instrumento = Instrumento.get({id: data.instrumento_id});
-		    $scope.consulta.instrumento = instrumento;
-		    $scope.consulta.oferta_academica = data.oferta_academica;
+		    Instrumento.get({id: data.instrumento_id}, function(instrumento, getResponseHeaders){
+			    $scope.consulta.instrumento = instrumento;
+			    $scope.consulta.oferta_academica = data.oferta_academica;
+
+		        console.log("después de cargar estructura");
+		        console.log($scope.respuesta.respuestas);
+	        });
 		})
     	.error(function(data, status, headers, config) {
     		console.log(data);
@@ -35,14 +38,22 @@ angular.module('sedadApp')
 			// or server returns response with an error status.
 		});
 
-    $scope
-
     $scope.responderConsulta = function() {
     	$scope.respuesta.cedula_estudiante = u.cedula;
 
+	    // Recrear preguntas de instrumetno en estructura de respuestas
+	    for(var bi = 0, nb = $scope.consulta.instrumento.bloques.length; bi < nb; bi++) {
+            for(var pi = 0, np = $scope.consulta.instrumento.bloques[bi].preguntas.length; pi < np; pi++) {
+            	var pregunta = $scope.consulta.instrumento.bloques[bi].preguntas[pi];
+            	if(pregunta.respuesta !== null && typeof pregunta.respuesta !== 'undefined') {
+	            	$scope.respuesta.respuestas.splice(pregunta.id,0,{ "pregunta_id": pregunta.id, "valor": pregunta.respuesta});
+	            }
+            }
+        }
+    	
     	console.log($scope.respuesta);
 
-	   /* $http.post(SEDAD_API_V1_URL + '/consultas/responder', $scope.respuesta)
+	   	$http.post(SEDAD_API_V1_URL + '/consultas/' + $stateParams.id + '/responder', { "consulta": $scope.respuesta })
 	    	.success(function(data, status, headers, config) {
 	    		console.log(data);
 	    		console.log("Consulta respondida");
@@ -51,6 +62,6 @@ angular.module('sedadApp')
 	    		console.log(data);
 				// called asynchronously if an error occurs
 				// or server returns response with an error status.
-			});*/
+			});
 	};
   }]);
