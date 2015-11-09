ab02c71a87815019a286b832cc084400adb320ad
Marco Gómez <marcogonzalo@gmail.com>
2015-11-08 20:34:14 -0430

    Buscar usuario

--- app/scripts/controllers/usuarios/usuarios.edit.js
+++ app/scripts/controllers/usuarios/usuarios.edit.js
@@ -39,8 +39,16 @@ angular.module('sedadApp')
     
     $http.get(SEDAD_API_V1_URL + '/usuarios/' + $stateParams.id + '/editar')
       .success(function(data, status, headers, config) {
-        $scope.usuarioEditar = data;
-        console.log($scope.usuarioEditar);
-        cruzarRol(data.usuario.rol_id);
+        if(data.usuario !== null) {
+          $scope.usuarioEditar = data;
+          console.log($scope.usuarioEditar);
+          cruzarRol(data.usuario.rol_id);
+        }
+        else {
+          Notification.error('No se pudo obtener el usuario');
+          $state.go('usuarios.index');
+        }
+      })
+      .error(function(data, status, headers, config) {
       });
   }]);
