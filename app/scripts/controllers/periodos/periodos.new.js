1b11acc4f9d718be45a335541407e98146891c37
Marco Gómez <marcogonzalo@gmail.com>
2015-11-08 20:33:37 -0430

    ajustes en notificaciones al gestionar periodos

--- app/scripts/controllers/periodos/periodos.new.js
+++ app/scripts/controllers/periodos/periodos.new.js
@@ -9,21 +9,21 @@ angular.module('sedadApp')
 		$scope.guardar = function() {
 			Periodo.save({ periodo_academico: { periodo: $scope.nuevoPeriodo, instrumento_id: $scope.instrumentoSeleccionado.id } }, function(data) {
                 if(data.estatus == "OK") {
-                    console.log(data);
 	                Notification.success("Período registrado");
 					$state.go('main');
                 }
                 else {
                 	Notification.error("No se pudo sincronizar el período")
-                    console.log(data);
+                	console.log(data);
                 }
             },
             function(error) {
-            	if(error.estatus == 304) {
+                if(error.status == 304) {
 	                Notification.warning("Este período ya se encuentra registrado");
                 }
                 else {
                 	Notification.error("No se pudo sincronizar el período")
+                	console.log(data);
                 }
             });
 		};
